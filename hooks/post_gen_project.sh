e6333ff467b32d4f7d35ee5cde5c425bc22e3658
Lasse Lambrechts <lasse.lambrechts@bt.no>
2023-01-17 12:10:41 +0100

    speeding up installation
    Signed-off-by: Lasse Lambrechts <lasse.lambrechts@bt.no>

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ## This post project generation script only runs if poetry is on the machine
-command -v poetry >/dev/null 2>&1 || { echo >&2 "poetry not found.  Aborting startup script."exit 1}
+if ! command -v poetry &> /dev/null
+then
+    echo "poetry not found. Read instructions for installing and try again. Aborting startup script."
+    exit 1
+fi
 
 # install necessary dependencies
 poetry install
@@ -24,19 +28,20 @@ mv env .env
   poetry install --with ssb
   curl -o etl/etl_ssb_api.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/etl_ssb_api.ipynb
   curl -o eda/research.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/eda-ssb.ipynb
-  poetry add pyjstat
 {% endif %}
 
 {% if cookiecutter.template == 'google sheets' %}
   poetry install --with google
   curl -o etl/01-etl.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/etl-gsheet.ipynb
   curl -o eda/research.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/generic-header.ipynb
+  poetry install pakkenellik["gspread"]
 {% endif %}
 
 {% if cookiecutter.template == 'gis' %}
   poetry install --with gis
   curl -o etl/01-etl.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/etl-gis.ipynb
   curl -o eda/research.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/generic-header.ipynb
+  poetry install pakkenellik["gis"]
 {% endif %}
 
 {% if cookiecutter.template == 'screen scraping' %}
@@ -49,8 +54,11 @@ mv env .env
   poetry install --with nvdb
   curl -o etl/01-etl.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/etl-nvdb.ipynb
   curl -o eda/research.ipynb https://raw.githubusercontent.com/BergensTidende/bord4-analysis-templates/master/templates/eda-nvdb.ipynb
+  poetry install pakkenellik["nvdb"]
 {% endif %}
 
 {% if cookiecutter.report_creation == true %}
   poetry install --with report
 {% endif %}
+
+echo "Your analysis project is available in the folder {{ cookiecutter.project_slug }}. \n Run 'cd {{ cookiecutter.project_slug }}' and 'make lab' to start working."
